59d93c4c3d57dfe2975824653d8869470cf27d70
izequiel <izequiel.r.alves@gmail.com>
2026-08-24 23:59:05 -0300

    adiciona e modifica pastas de exercícios de Métodos

---
+++ Emprestimo.java
@@ -0,0 +1,44 @@
+package estudos.Izequiel.metódos;
+
+/**
+ * Classe de exemplo para o exercício da Aula 1 do Métodos.
+ */
+
+public class Emprestimo {
+
+    public static int getDuasParcelas() {
+        return 2;
+    }
+
+    public static int gitTresParcelas() {
+        return 3;
+    }
+
+    public static double getTaxaDuasParcelas() {
+        return 0.3;
+    }
+
+    public static double getTaxaTresParcelas() {
+        return 0.45;
+    }
+
+    public static void calcular(double valor, int parcelas) {
+
+        if (parcelas == 2) {
+
+            double valorFinal = valor + (valor * getTaxaDuasParcelas());
+
+            System.out.println("Valor final do empréstimo para 2 parcelas: R$ " + valorFinal);
+
+        } else if (parcelas == 3) {
+
+            double valorFinal = valor + (valor * getTaxaTresParcelas());
+
+            System.out.println("Valor final do empréstimo para 3 parcelas: R$ " + valorFinal);
+
+        }else {
+            System.out.println("Valor de parcela não aceita");
+        }
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,26 @@
+package estudos.Izequiel.metódos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nExercícios Calculadora");
+            Calculadora.soma(10, 20);
+            Calculadora.subtracao(40, 10);
+            Calculadora.multiplicacao(10, 3);
+            Calculadora.divisao(60, 2);
+
+        System.out.println("\nExercícios Mensagem");
+            Mensagem.obterMensagem(5);
+            Mensagem.obterMensagem(20);
+            Mensagem.obterMensagem(15);
+
+        System.out.println("\nExercícios Empréstimo");
+            Emprestimo.calcular(1000,Emprestimo.getDuasParcelas());
+            Emprestimo.calcular(1000,Emprestimo.gitTresParcelas());
+            Emprestimo.calcular(1000, 5);
+
+
+
+    }
+}
